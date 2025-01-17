2760fc3eea9a0d86cea0e2a33ac70fea25154a2c
otalavera06 <oie_tal_mun@goierrieskola.org>
2025-01-17 10:03:58 +0100

    Footer aldaketak refaktorizazioa

---
+++ footer.php
@@ -0,0 +1,28 @@
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<div class="footer">
+                <p> &copy; Second Life</p>
+            </div>
+
+            <div class="kookiak">
+                <p>Cookie Politikak </p>
+
+                <div class="kookietestua">
+                    <p>Second Life-n, zure nabigazio esperientzia hobetzeko,
+                        gure webgunearen erabilera aztertzeko eta eskaintzen dizkizugun edukia pertsonalizatzeko
+                        cookie-ak erabiltzen ditugu. Gure webgunean jarraituz,
+                        cookie-ak erabiltzea onartzen duzu gure cookie politikarekin bat etorriz.</p>
+
+                    <p>Cookie-en lehentasunak edozein unetan kudeatu ditzakezu zure nabigatzailearen konfigurazioaren
+                        bidez.
+                        Erabiltzen ditugun cookie-ak eta nola kontrolatu ditzakezun jakiteko, kontsultatu gure
+                        Pribatutasun Politika.</p>
+                </div>
+                <div class="kookiebotoia">
+                    <input type="submit" name="kookiak" value="Onartu">
+                </div>
+            </div>
---
+++ header.php
@@ -0,0 +1,12 @@
+<div>
+    <p class="izenburua">Second Life</p>
+</div>
+<div>
+    <div class="menua">
+        <a href="hasiera.php">Sarrera</a>
+        <a href="zerbitzuak.php">Zerbitzuak</a>
+        <a href="kontaktatu.php">Kontaktatu</a>
+        <a href="denda.php">Denda</a>
+        <a href="lagundu.php">Lagundu!</a>
+    </div>
+</div>
